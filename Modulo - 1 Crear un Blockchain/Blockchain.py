1aee4a0097b68ce3252e9d460f3c65ba0fcf3c78
GabrielGJU7 <gjacoboulloa@gmail.com>
2023-11-13 17:09:18 -0600

    Creación de métodos de bloques

--- Modulo - 1 Crear un Blockchain/Blockchain.py
+++ Modulo - 1 Crear un Blockchain/Blockchain.py
@@ -1,7 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
+"""
+@Creado el: 13/11/2023
+
+@Autor: Gabriel Jacobo Ulloa
+
 """
 
+
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.createBlock(proof = 1, previous_hash = '0')
+        
+    def createBlock(self, proof, previous_hash):
+        block = {'index' : len(self.chain) + 1,
+                 'timestamp' : str(datetime.datetime.now()),
+                 'proof' : proof,
+                 'previous_hash' : previous_hash }
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chan[-1]
+        
